f3979c76e97525285a76bc8e2c2d4ffb66cca3ac
Rowdy Rabouw <rowdy@double-r.nl>
2022-06-06 11:25:43 +0200

    feat: add @rowdy.rabouw/video-component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "@rowdy.rabouw/video-component";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <video-component vimeo="705066644"></video-component>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
